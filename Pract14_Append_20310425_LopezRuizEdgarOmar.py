2986b8e436235126937e19024d19640151cbe3b3
unknown <a20310425@ceti.mx>
2023-03-14 02:19:43 -0600

    Create Pract14_Append_20310425_LopezRuizEdgarOmar.py

---
+++ Pract14_Append_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+numeros.append('diez')
+numeros.append('once')
+print(numeros)
