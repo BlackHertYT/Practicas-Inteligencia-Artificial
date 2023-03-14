fa8e78f8d846437076961dd21b1b76f54d919827
unknown <a20310425@ceti.mx>
2023-03-14 02:19:49 -0600

    Create Pract12_Remove_20310425_LopezRuizEdgarOmar.py

---
+++ Pract12_Remove_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+numeros.remove('dos')
+numeros.remove('ocho')
+print(numeros)
