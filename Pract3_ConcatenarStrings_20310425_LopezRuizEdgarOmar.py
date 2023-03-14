4a6d75d0153bfbcc3407d7c39e24d7401f49792f
unknown <a20310425@ceti.mx>
2023-03-14 02:18:35 -0600

    Create Pract3_ConcatenarStrings_20310425_LopezRuizEdgarOmar.py

---
+++ Pract3_ConcatenarStrings_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+string1 = "Hola" 
+string2 = "Mundo"
+string3 = string1 + " " + string2 
+print(string3)
+
+string1 = string1 + " " + string2 
+print(string1)
+
+print("Hola " + string2 + " Arriba papito Musk") 
