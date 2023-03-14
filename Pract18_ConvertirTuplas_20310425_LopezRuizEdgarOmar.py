8f964b6f9e22d662eebf9b691d32e38faa8c433f
unknown <a20310425@ceti.mx>
2023-03-14 02:19:26 -0600

    Create Pract18_ConvertirTuplas_20310425_LopezRuizEdgarOmar.py

---
+++ Pract18_ConvertirTuplas_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 19:01:33 2023
+
+@author: guill
+"""
+
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+numeros_T= tuple(numeros)
+print(type(numeros_T))
