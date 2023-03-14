c104c68e92aa45b7e05c8760f01c8c6dba0834b3
unknown <a20310425@ceti.mx>
2023-03-14 02:19:40 -0600

    Create Pract15_Insert_20310425_LopezRuizEdgarOmar.py

---
+++ Pract15_Insert_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+numeros.insert(5,'Derf')
+numeros.insert(11,'uno')
+print(numeros)
