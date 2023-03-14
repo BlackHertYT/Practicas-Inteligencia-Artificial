d43a8050d99d23fe9060140d8c505f20a8c8878f
unknown <a20310425@ceti.mx>
2023-03-14 02:18:17 -0600

    Create Pract9_Listas_20310425_LopezRuizEdgarOmar.py

---
+++ Pract9_Listas_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+colores = ["rojo", "azul", "verde", "amarillo", "rosa"]
+print(colores[3])
+print(colores.index("azul"))
+print(colores.index("rojo"))
+Numeros = ["nueve","dos","seis","cuatro","uno"]
+print(Numeros)
