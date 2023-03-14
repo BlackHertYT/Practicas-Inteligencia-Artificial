b30050d70761bf7f4aa81444b2182df4aef42e31
unknown <a20310425@ceti.mx>
2023-03-14 02:19:37 -0600

    Create Pract16_Sort_20310425_LopezRuizEdgarOmar.py

---
+++ Pract16_Sort_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+numeros.sort(reverse = True)
+print(numeros)
