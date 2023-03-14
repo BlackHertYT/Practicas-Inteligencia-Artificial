a777a328305b1f31d599f0bd3dfc5ad9c936393f
unknown <a20310425@ceti.mx>
2023-03-14 02:19:13 -0600

    Create Pract22_ComprobarDatos_20310425_LopezRuizEdgarOmar.py

---
+++ Pract22_ComprobarDatos_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+numeros = int 1,2,3,4:
+
+entrada = input('Escribe un numero del 1 al 4 ...\n')
+
+if(entrada in numeros):
+    print('El numero '+entrada+ ' está dentro de los valores')
+else:
+    print('El numero '+entrada+ ' no está esta dentro de los valores')
