eec0caee9ea306991acee8c3ec82bc5a4b5de691
unknown <a20310425@ceti.mx>
2023-03-14 02:18:40 -0600

    Create Pract30_MetodosdeDiccionara_20310425_LopezRuizEdgarOmar.py

---
+++ Pract30_MetodosdeDiccionara_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+def suma(numero1 , numero2):
+    print(str(x+y))
+    
+x =20
+y= 10
+suma(x,y)
+y=30
+suma(x,y)
+x=50000
+y=7000
+suma(x,y)
