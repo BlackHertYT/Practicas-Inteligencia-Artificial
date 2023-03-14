f44fa3a3a60c969a306c93ae5a21646d5c35167d
unknown <a20310425@ceti.mx>
2023-03-14 02:18:44 -0600

    Create Pract2_Strings_20310425_LopezRuizEdgarOmar.py

---
+++ Pract2_Strings_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+Saludo = "Mi primer Hola mundo"
+print(Saludo)
+Saludo1 = '"print()" esta funcion se utiliza para imprimir los textos'
+print(Saludo1)
