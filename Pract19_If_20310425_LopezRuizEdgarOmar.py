5ad9e66d86a1a9d5ea991fef6c8c89183ec1fc23
unknown <a20310425@ceti.mx>
2023-03-14 02:19:23 -0600

    Create Pract19_If_20310425_LopezRuizEdgarOmar.py

---
+++ Pract19_If_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+num1 = 35
+num2 = 50
+
+if num1 != num2:
+	print('Si se cumple la afirmacion 1')
+    
+num3 = 89
+num4 = 55
+
+if num3 > num4:
+	print('Si se cumple la afirmacion 2')
+    
+num5 = 67
+num6 = 67
+
+if num5 != num6:
+	print('Si se cumple la afirmacion 3')
