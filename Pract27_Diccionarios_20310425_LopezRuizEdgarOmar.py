fb3dd5ab01b7549d447b26f1b821541c851a6e80
unknown <a20310425@ceti.mx>
2023-03-14 02:18:56 -0600

    Create Pract27_Diccionarios_20310425_LopezRuizEdgarOmar.py

---
+++ Pract27_Diccionarios_20310425_LopezRuizEdgarOmar.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+audifono1 = {
+	'Categoría': 'Audifonos',
+	'Modelo': 'Samsung Ear buds Pro',
+	'Precio': '120,00'
+}
+
+audifono2 = {
+	'Categoría': 'Audifonos',
+	'Modelo': 'Air pods pro',
+	'Precio': '160,00'
+}
+
+consulta = audifono1['Modelo']
+print(audifono2['Modelo'])
+print(audifono2['Precio'])
