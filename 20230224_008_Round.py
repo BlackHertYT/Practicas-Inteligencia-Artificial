15bbf2a7e8aab564c585afe4d21e97e0a1d3d7ea
unknown <a20310425@ceti.mx>
2023-03-16 01:38:18 -0600

    Create 20230224_008_Round.py

---
+++ 20230224_008_Round.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+# Se define una variable llamada "Redondear" y se le asigna el valor de un número con decimales.
+Redondear = 3.14163224566432134
+
+# Se utiliza la función "round()" para redondear el número a 5 decimales.
+# El resultado del redondeo se almacena nuevamente en la variable "Redondear".
+Redondear = round(Redondear, 5)
+
+# Se utiliza la función "print()" para imprimir el resultado del redondeo en la consola.
+print(Redondear)
