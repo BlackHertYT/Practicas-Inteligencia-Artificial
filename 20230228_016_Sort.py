3705a355373803a6567c3588fd6fe78cd48ca520
unknown <a20310425@ceti.mx>
2023-03-16 01:38:42 -0600

    Create 20230228_016_Sort.py

---
+++ 20230228_016_Sort.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#Este programa crea una lista llamada "numeros" que contiene diez cadenas de texto.
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+#Luego, se utiliza la función "sort" con el parámetro "reverse = True" para ordenar la lista "numeros" en orden descendente (de la 'z' a la 'a') alfabético.
+numeros.sort(reverse = True)
+print(numeros)
