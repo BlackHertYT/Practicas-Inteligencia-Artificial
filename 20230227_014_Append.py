6df04b72729eabdd100cdc537f2442c49ffc0652
unknown <a20310425@ceti.mx>
2023-03-16 01:38:36 -0600

    Create 20230227_014_Append.py

---
+++ 20230227_014_Append.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#Este programa crea una lista llamada "numeros" que contiene diez cadenas de texto.
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+#Luego, se utiliza la función "append" para agregar los elementos 'diez' y 'once' al final de la lista "numeros".
+numeros.append('diez')
+numeros.append('once')
+print(numeros)
