f2754730c0570f16153d4a960b1d3097827aa4f8
unknown <a20310425@ceti.mx>
2023-03-16 01:39:59 -0600

    Create 20230307_030_Funciones.py

---
+++ 20230307_030_Funciones.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#Este programa define una función llamada suma que recibe dos argumentos numero1 y numero2. Luego, en la función, los dos números se suman y se imprime el resultado.
+def suma(numero1 , numero2):
+    print(str(x+y))
+    
+    #Después se crean tres variables x, y, y se les asignan valores. La función suma se llama tres veces con diferentes valores de x e y.
+#La primera llamada suma 20 y 10, lo cual resulta en 30. La segunda llamada suma 20 y 30, lo cual resulta en 50. La última llamada suma 50000 y 7000, lo cual resulta en 57000.
+x =20
+y= 10
+suma(x,y)
+y=30
+suma(x,y)
+x=50000
+y=7000
+suma(x,y)
