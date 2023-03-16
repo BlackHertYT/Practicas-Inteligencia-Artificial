647381882ff790c6f98d3dd79f745d48dcd6d183
unknown <a20310425@ceti.mx>
2023-03-16 01:39:42 -0600

    Create 20230306_029_MetodosdeDiccionaro.py

---
+++ 20230306_029_MetodosdeDiccionaro.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+audifono1 = {
+	'Categoría': 'Audifonos',
+	'Modelo': 'Samsung Ear buds Pro',
+	'Precio': '120,00'
+}
+
+audifono2 = {
+	'Categoría': 'Audifonos',
+	'Modelo': 'Air pods pro',
+	'Precio': '160,00'
+}
+del audifono1
+del audifono2['Categoría']
+del audifono2['Precio']
+print (audifono1['Modelo'])
