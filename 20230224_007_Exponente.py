075f0a5f91e393df34d22f8e86b6ba00e3a9656b
unknown <a20310425@ceti.mx>
2023-03-16 01:38:16 -0600

    Create 20230224_007_Exponente.py

---
+++ 20230224_007_Exponente.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+# Se utiliza una operación matemática para calcular un número elevado a la sexta potencia.
+# El resultado se almacena en una variable llamada "Exponente".
+Exponente = 3*3*3*3*3*3
+
+# Se utiliza la función "print()" para imprimir el resultado del cálculo en la consola.
+print(Exponente)
