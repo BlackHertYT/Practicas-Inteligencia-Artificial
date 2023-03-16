b4e8c2e8aba3edf87c8f1c1e0b1e48c7ba4beb55
unknown <a20310425@ceti.mx>
2023-03-16 01:38:07 -0600

    Create 20230222_004_UpperLowerTitle.py

---
+++ 20230222_004_UpperLowerTitle.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+# Se define la variable "Nombre" con el valor "BlackHertYT".
+Nombre = "BlackHertYT"
+# Se aplica el método "title()" a la variable "Nombre" para convertir la primera letra de cada palabra a mayúscula.
+Nombre = Nombre.title()
+# Se imprime el valor de la variable "Nombre".
+print(Nombre)
+
+# Se define la variable "Funcion1" con el valor "Con esta función haces todas las letra minusculas".
+Funcion1 = "Con esta función haces todas las letra minusculas"
+# Se aplica el método "lower()" a la variable "Funcion1" para convertir todas las letras a minúscula.
+Funcion1 = Funcion1.lower()
+# Se imprime el valor de la variable "Funcion1".
+print(Funcion1)
+
+# Se define la variable "Funcion2" con el valor "Con esta función haces todas las letras mayusculas".
+Funcion2 = "Con esta función haces todas las letras mayusculas"
+# Se aplica el método "upper()" a la variable "Funcion2" para convertir todas las letras a mayúscula.
+Funcion2 = Funcion2.upper()
+# Se imprime el valor de la variable "Funcion2".
+print(Funcion2)
