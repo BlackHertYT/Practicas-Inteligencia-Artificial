df1f71c3217f01399db9d6215838c310d1aab95f
unknown <a20310425@ceti.mx>
2023-03-16 01:38:11 -0600

    Create 20230223_005_SaltosDeLineaTabulaciones.py

---
+++ 20230223_005_SaltosDeLineaTabulaciones.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+# Se utiliza la función "print()" para imprimir una lista de lenguajes de programación.
+# Cada lenguaje se muestra en una línea separada por el caracter "\n".
+# Además, se utiliza el caracter "-" para dar formato a cada línea de la lista.
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.\n-C.\n-Basic.")
