ff7bd1d794897bf6201128e8de65f5f5a7b1dd4c
unknown <a20310425@ceti.mx>
2023-03-16 01:38:44 -0600

    Create 20230301_017_Tupla.py

---
+++ 20230301_017_Tupla.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#Este programa crea una lista llamada "numeros" que contiene diez cadenas de texto.
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+print(numeros[1])
+#A continuación, se crea una tupla llamada "numeros2" que contiene cuatro valores enteros.
+numeros2 = (25, 6, 9 , 32)
+print(numeros2[3]-numeros2[2]-numeros2[1]+numeros2[0])
