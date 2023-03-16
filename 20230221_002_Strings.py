6d0dc7a33c0ee9f3753850bb2ba6137aafc5cd61
unknown <a20310425@ceti.mx>
2023-03-16 01:38:02 -0600

    Create 20230221_002_Strings.py

---
+++ 20230221_002_Strings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+# Se define la variable "Saludo" con el valor "Mi primer Hola mundo".
+Saludo = "Mi primer Hola mundo"
+# Se imprime el valor de la variable "Saludo".
+print(Saludo)
+
+# Se define la variable "Saludo1" con el valor ""print()" esta función se utiliza para imprimir los textos".
+Saludo1 = '"print()" esta función se utiliza para imprimir los textos'
+# Se imprime el valor de la variable "Saludo1".
+print(Saludo1)
