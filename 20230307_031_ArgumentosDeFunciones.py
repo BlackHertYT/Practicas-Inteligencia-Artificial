0c4fe7e5efc468174a9333f37dc90243542f8d28
unknown <a20310425@ceti.mx>
2023-03-16 01:39:55 -0600

    Create 20230307_031_ArgumentosDeFunciones.py

---
+++ 20230307_031_ArgumentosDeFunciones.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: omar
+"""
+
+def suma(numero1 , numero2):
+    print(str(x+y))
+#El programa define una función llamada "suma" que toma dos argumentos: "numero1" y "numero2". Luego, se establecen valores para las variables "x" e "y". Se llama a la función "suma" con los valores actuales de "x" e "y". La función suma los dos números y los imprime en la consola como una cadena de caracteres.
+x =20
+y= 10
+suma(x,y)
+y=30
+#Luego se cambia el valor de la variable "y" y se llama a la función "suma" de nuevo. Se repite el proceso, cambiando los valores de "x" e "y" y llamando a la función "suma" con estos nuevos valores.
+suma(x,y)
+x=50000
+y=7000
+suma(x,y)
