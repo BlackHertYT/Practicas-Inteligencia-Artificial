42d4cc7a225c6c9835f397228edac6660fe1c666
unknown <a20310425@ceti.mx>
2023-03-16 01:38:39 -0600

    Create 20230228_015_Insert.py

---
+++ 20230228_015_Insert.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#Este programa crea una lista llamada "numeros" que contiene diez cadenas de texto.
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+#Luego, se utiliza la función "insert" para agregar el elemento 'Derf' en la posición 5 de la lista "numeros", desplazando los elementos a la derecha a partir de esa posición.
+numeros.insert(5,'Derf')
+#A continuación, se utiliza la función "insert" para agregar el elemento 'uno' en la posición 11 de la lista "numeros", desplazando los elementos a la derecha a partir de esa posición.
+numeros.insert(11,'uno')
+print(numeros)
