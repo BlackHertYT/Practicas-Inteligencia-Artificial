22eecb4351a7bf98607ca6af40c2b89999d6568e
unknown <a20310425@ceti.mx>
2023-03-16 01:37:59 -0600

    Create 20230221_001_Variables.py

---
+++ 20230221_001_Variables.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 23:44:05 2023
+
+@author: Omar
+"""
+# Se define la variable "saludo" con el valor "Hola mundo".
+saludo = "Hola mundo"
+# Se imprime el valor de la variable "saludo".
+print(saludo)
+
+# Se define la variable "x" con el valor 2.
+x = 2
+# Se define la variable "y" con el valor 2.
+y = 2
+# Se realiza la suma de las variables "x" y "y" y se asigna el resultado a la variable "z".
+z = x + y
+# Se imprime el valor de la variable "z".
+print(z)
