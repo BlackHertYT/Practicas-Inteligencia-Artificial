b568524d87c5c5e5557376dc9de787bf59591eb3
unknown <a20310425@ceti.mx>
2023-03-16 01:38:56 -0600

    Create 20230302_020_IfElse.py

---
+++ 20230302_020_IfElse.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#El programa define una variable llamada "numero" y le asigna el valor de 13. 
+numero = 13
+
+#Luego, verifica si el valor de "numero" es igual a 13 usando una estructura de control "if". Si el valor de "numero" es igual a 13, el programa imprime "El numero es 13." en la consola. De lo contrario, el programa imprime "El numero es otro numero".
+if numero == 13:
+    print("El numero es 13.")
+else:
+    print("El numero es otro numero")
