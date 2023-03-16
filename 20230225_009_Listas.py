55bd008414c8c1dc95f2b881a341edbb4d6fcbbd
unknown <a20310425@ceti.mx>
2023-03-16 01:38:21 -0600

    Create 20230225_009_Listas.py

---
+++ 20230225_009_Listas.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+# Se define una lista llamada "colores" que contiene varios elementos de tipo string.
+colores = ["rojo", "azul", "verde", "amarillo", "rosa"]
+
+# Se utiliza la sintaxis de indexación para acceder al elemento en la posición 3 de la lista "colores".
+# El elemento es "amarillo" porque las listas en Python empiezan a contar desde 0.
+print(colores[3])
+
+# Se utiliza el método "index()" para buscar la posición del elemento "azul" en la lista "colores".
+# El resultado es 1, que es la posición del elemento en la lista.
+print(colores.index("azul"))
+
+# Se utiliza el método "index()" para buscar la posición del elemento "rojo" en la lista "colores".
+# El resultado es 0, que es la posición del elemento en la lista.
+print(colores.index("rojo"))
+
+# Se define una nueva lista llamada "Numeros" que contiene varios elementos de tipo string.
+Numeros = ["nueve","dos","seis","cuatro","uno"]
+
+# Se utiliza la función "print()" para imprimir la lista "Numeros" en la consola.
+print(Numeros)
