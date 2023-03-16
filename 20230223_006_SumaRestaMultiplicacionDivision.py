fa1207cffa584514698177455dc1247488afe917
unknown <a20310425@ceti.mx>
2023-03-16 01:38:13 -0600

    Create 20230223_006_SumaRestaMultiplicacionDivision.py

---
+++ 20230223_006_SumaRestaMultiplicacionDivision.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+
+Suma = 2+2
+print(Suma)
+
+Resta = 3-4
+print(Resta)
+
+Multiplicacion = 20*4
+print(Multiplicacion)
+
+Division =(5000/5)
+print(Division)
+
+Operacion = (9+11)/(4-2)
+print(Operacion)
