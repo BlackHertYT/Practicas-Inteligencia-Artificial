3d12ed80c987f37f1ba0e32da22fe1d0c8968d89
unknown <a20310425@ceti.mx>
2023-03-16 01:38:25 -0600

    Create 20230225_010_PosicionesNegativas.py

---
+++ 20230225_010_PosicionesNegativas.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#Aquí se crea una lista llamada "numeros" que contiene diez cadenas de texto.
+#Luego, se utiliza la función "print" para imprimir varias cadenas de texto de la lista "numeros"
+#Cada cadena de texto se separa por un salto de línea ("\n") para imprimirse en líneas separadas en la consola.
+
+numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
+print(numeros[-5]+"\n" + numeros[-1] + "\n" + numeros[-2] + "\n"+numeros[-7] + "\n" + numeros[-6])
