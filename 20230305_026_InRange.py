186d507e09bbaf770e2abef0a7765ec4631d23a3
unknown <a20310425@ceti.mx>
2023-03-16 01:39:32 -0600

    Create 20230305_026_InRange.py

---
+++ 20230305_026_InRange.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Omar
+"""
+#El programa imprime los valores en un rango de números, empezando desde 10 hasta 1000, con incrementos de 50. En cada iteración del bucle, se imprime el valor actual de la variable "x".
+for x in range(10, 1000, 50):
+	print(x)
