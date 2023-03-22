b1c99097b5b89836d020505595f138f979db9f63
BlackHertYT <127716607+BlackHertYT@users.noreply.github.com>
2023-03-21 20:32:03 -0600

    Delete Pract10_PosicionesNegativas_20310425_LopezRuizEdgarOmar.py

--- Pract10_PosicionesNegativas_20310425_LopezRuizEdgarOmar.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-
-
-@author: Omar
-"""
- 
-numeros = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'cero']
-print(numeros[-5]+"\n" + numeros[-1] + "\n" + numeros[-2] + "\n"+numeros[-7] + "\n" + numeros[-6])
